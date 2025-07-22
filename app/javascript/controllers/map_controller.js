481c15b07e9caab197fa2508ec67d015f1e97de8
regonn <programmer@haizine.art>
2025-07-22 14:45:37 +0000

    leaflet を温泉一覧画面に表示

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,52 @@
 import { Controller } from "@hotwired/stimulus"
+import L from "leaflet"
 
-// Connects to data-controller="maps"
+// Connects to data-controller="map"
 export default class extends Controller {
+  static targets = ["container"]
+  static values = { onsens: Array }
+
   connect() {
+    this.onsens = this._parseOnsensData();
+    console.log(this.onsens);
+
+    var map = L.map(this.containerTarget).setView([35.468, 133.0483], 11.5);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const onsenIcon = L.icon({
+      iconUrl: '/onsen.svg',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+    });
+
+    this.onsens.forEach(onsen => {
+      L.marker([onsen.geo_lat, onsen.geo_lng], { icon: onsenIcon })
+        .addTo(map)
+        .bindPopup(onsen.name);
+    });
+  }
+
+  disconnect() {
+    this.map.remove();
+  }
+
+  // === プライベートメソッド（内部処理用） ===
+
+  /**
+   * HTML要素から温泉データを取得・パース
+   * @returns {Array} 温泉データの配列
+   */
+  _parseOnsensData() {
+    try {
+      const rawData = this.element.dataset.mapOnsens || "[]";
+      return JSON.parse(rawData);
+    } catch (error) {
+      console.warn("温泉データのパースに失敗:", error);
+      return [];
+    }
   }
 }
